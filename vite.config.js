a0ec30f251ee67423954533d216b64b389ac6fdf
Marsuazo23/ <marcesuazo2004@gmail.com>
2025-04-07 22:26:40 -0600

    Actualización del archivo vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,46 +6,41 @@ import handlebars from 'vite-plugin-handlebars'
 import {ViteMinifyPlugin} from 'vite-plugin-minify'
 import { getPageContext } from './data/index'
 
-const pageContext = (pagePath) => {
-    return getPageContext(pagePath)
-}
-
-const obtenerEntradas = () => {
-    // Busca en la raíz del proyecto
-    const entradas = glob.sync('./**/*.html', {
-        ignore: [
-            './dist/**',
-            './node_modules/**'
-        ]
-    });
-
-    if (entradas.length === 0) {
-        throw new Error('No se encontraron archivos HTML para compilar. Verifica las rutas.');
-    }
-
+const obtenerEntradas = ()=>{
     return Object.fromEntries(
-        entradas.map(file => [
-            // Mantén la estructura de directorios relativa
-            path.relative('./', file.slice(0, file.length - path.extname(file).length)),
-            resolve(__dirname, file)
-        ])
+        [
+            ...glob.sync(
+                './**/*.html',
+                {
+                    ignore: [
+                        './dist/**',
+                        './node_modules/**'
+                    ]
+                }
+            ).map(
+                fileData => [
+                    fileData.slice(0, fileData.length - path.extname(fileData).length),
+                    resolve(__dirname, fileData)
+                ]
+            )
+        ]
     );
 }
 
+
 export default defineConfig({
     appType: 'mpa',
-    base: process.env.DEPLOY_BASE_URL ? `/${process.env.DEPLOY_BASE_URL.replace(/^\/|\/$/g, '')}/` : '/',
+    base: process.env.DEPLOY_BASE_URL,
     build: {
         rollupOptions: {
             input: obtenerEntradas()
         },
-        minify: true,
-        emptyOutDir: true
+        minify: true
     },
     plugins: [
         handlebars({
             partialDirectory: resolve(__dirname, 'partials'),
-            context: pageContext
+            context : getPageContext
         }),
         htmlPurge({}),
         ViteMinifyPlugin()
