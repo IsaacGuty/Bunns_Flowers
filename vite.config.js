384b5c9a3bd191cd9517b34db2cb57ab13b2b3c7
Marsuazo23/ <marcesuazo2004@gmail.com>
2025-04-07 18:31:44 -0600

    Actualización del archivo vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,34 @@
-import path, {resolve} from 'node:path'
+import path, { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import * as glob from 'glob'
 import htmlPurge from 'vite-plugin-purgecss'
 import handlebars from 'vite-plugin-handlebars'
-import {ViteMinifyPlugin} from 'vite-plugin-minify'
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
 import { getPageContext } from './data/index'
 
-const obtenerEntradas = ()=>{
-    return Object.fromEntries(
-        [
-            ...glob.sync(
-                './**/*.html',
-                {
-                    ignore: [
-                        './dist/**',
-                        './node_modules/**'
-                    ]
-                }
-            ).map(
-                fileData => [
-                    fileData.slice(0, fileData.length - path.extname(fileData).length),
-                    resolve(__dirname, fileData)
-                ]
-            )
-        ]
-    );
+const pageContext = (pagePath) => {
+    return getPageContext(pagePath)
 }
 
+const obtenerEntradas = () => {
+    return Object.fromEntries(
+        glob.sync(
+            './pages/*/.html',
+            {
+                ignore: [
+                    './dist/**',
+                    './node_modules/**'
+                ]
+            }
+        ).map((fileData) => {
+            const entrada = path.relative('pages', fileData.slice(0, fileData.length - path.extname(fileData).length));
+            return [
+                entrada,
+                resolve(__dirname, fileData)
+            ];
+        })
+    );
+}
 
 export default defineConfig({
     appType: 'mpa',
@@ -40,7 +42,10 @@ export default defineConfig({
     plugins: [
         handlebars({
             partialDirectory: resolve(__dirname, 'partials'),
-            context : getPageContext
+            context: pageContext,
+            helpers: {
+                firstLetter: (str) => str.charAt(0).toUpperCase()
+            }
         }),
         htmlPurge({}),
         ViteMinifyPlugin()
