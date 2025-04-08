a08f2d93415a49f18cb837f144e0aa1816ad5a2e
Marsuazo23/ <marcesuazo2004@gmail.com>
2025-04-07 22:25:44 -0600

    Actualización de la carpeta data

--- data/pages/catalogoPage.js
+++ data/pages/catalogoPage.js
@@ -2,23 +2,24 @@ export const catalogoPage = {
     categorias: [
         {
             id: "rosas",
+            idCapitalized: "Rosas",
             nombre: "ROSAS",
             imagen: "imagenes/florcorazon.jpg",
             productos: [
                 {
-                    nombre: "Ramo de 3 rosas eternas",
+                    nombre: "Ramo",
                     imagen: "imagenes/ramorosas1.jpg",
                     precio: 100,
                     descripcion: "Ramos de 3 rosas eternas"
                 },
                 {
-                    nombre: "Ramo de 7 rosas eternas",
+                    nombre: "Ramo",
                     imagen: "imagenes/ramorosas2.jpg",
                     precio: 230,
                     descripcion: "Ramo de 7 rosas eternas"
                 },
                 {
-                    nombre: "Ramo de 15 rosas eternas",
+                    nombre: "Ramo",
                     imagen: "imagenes/ramorosas3.jpg",
                     precio: 410,
                     descripcion: "Ramo de 15 rosas eternas"
@@ -27,23 +28,24 @@ export const catalogoPage = {
         },
         {
             id: "girasoles",
+            idCapitalized: "Girasoles",
             nombre: "GIRASOLES",
             imagen: "imagenes/florgirasol.jpg",
             productos: [
                 {
-                    nombre: "Ramo de 1 girasol",
+                    nombre: "Ramo",
                     imagen: "imagenes/ramogirasol1.jpg",
                     precio: 60,
                     descripcion: "Ramo de 1 girasol"
                 },
                 {
-                    nombre: "Ramo de 7 rosas y 1 girasol",
+                    nombre: "Ramo",
                     imagen: "imagenes/ramogirasol2.jpg",
                     precio: 270,
                     descripcion: "Ramo de 7 rosas y 1 girasol"
                 },
                 {
-                    nombre: "Ramo de 10 rosas, 1 girasol y 10 chocolates",
+                    nombre: "Ramo",
                     imagen: "imagenes/ramogirasol3.jpg",
                     precio: 760,
                     descripcion: "Ramo de 10 rosas, 1 girasol y 10 chocolates"
@@ -52,23 +54,24 @@ export const catalogoPage = {
         },
         {
             id: "dalias",
+            idCapitalized: "Dalias",
             nombre: "DALIAS",
             imagen: "imagenes/flordhalia3.jpg",
             productos: [
                 {
-                    nombre: "Ramo de 5 dalias eternas",
+                    nombre: "Ramo",
                     imagen: "imagenes/flordhalia1.jpg",
                     precio: 240,
                     descripcion: "Ramo de 5 dalias eternas"
                 },
                 {
-                    nombre: "Ramo de 2 dalias, 1 rosa y 1 HotWheel",
+                    nombre: "Ramo",
                     imagen: "imagenes/flordhalia2.jpg",
                     precio: 260,
                     descripcion: "Ramo de 2 dalias, 1 rosa y 1 HotWheel"
                 },
                 {
-                    nombre: "Ramo de 7 dalias eternas",
+                    nombre: "Ramo",
                     imagen: "imagenes/flordhalia3.jpg",
                     precio: 320,
                     descripcion: "Ramo de 7 dalias eternas"
@@ -77,23 +80,24 @@ export const catalogoPage = {
         },
         {
             id: "tulipanes",
+            idCapitalized: "Tulipanes",
             nombre: "TULIPANES",
             imagen: "imagenes/flortulipan.jpeg",
             productos: [
                 {
-                    nombre: "Ramo de 5 tulipanes",
+                    nombre: "Ramo",
                     imagen: "imagenes/ramotulipanes1.jpeg",
                     precio: 160,
                     descripcion: "Ramo de 5 Tulipanes"
                 },
                 {
-                    nombre: "Ramo de 5 tulipanes con envoltorio",
+                    nombre: "Ramo",
                     imagen: "imagenes/ramotulipanes2.jpeg",
                     precio: 170,
                     descripcion: "Ramo de 5 Tulipanes con envoltorio (70 lempiras adicional cada Polaroid)"
                 },
                 {
-                    nombre: "Ramo de 5 Tulipanes y retrato",
+                    nombre: "Ramo",
                     imagen: "imagenes/ramotulipanes3.jpeg",
                     precio: 370,
                     descripcion: "Ramo de 5 Tulipanes y una retratera con 2 Polaroids"
